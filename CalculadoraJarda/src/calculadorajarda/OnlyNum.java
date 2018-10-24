d30872c938546b208ebe91940b95ed4d7f53c439
FelpsSilva <felipefreeak@gmail.com>
2018-10-24 13:59:06 -0300

    First commit

---
+++ CalculadoraJarda/src/calculadorajarda/OnlyNum.java
@@ -0,0 +1,18 @@
+
+package calculadorajarda;
+
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.PlainDocument;
+
+
+public class OnlyNum extends PlainDocument {
+   
+    @Override 
+   public void insertString(int offs, String str, AttributeSet a) throws BadLocationException {
+       super.insertString(offs, str.replaceAll("[^0-9.]", ""), a);
+   
+   }
+    
+  
+}
